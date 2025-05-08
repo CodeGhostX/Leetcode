f38a665feb7163fd8ef58085ec088d692500e2f0
rajat-28 <rajatgarg0903@gmail.com>
2025-05-08 13:34:38 +0530

    feat(forget_pass) controllers added

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,5 +1,10 @@
 import { User } from '../models/index.js'
 import bcrypt from 'bcrypt'
+import ErrorResponse from '../utils/common/error.js';
+import { StatusCodes } from 'http-status-codes';
+import SuccessResponse from '../utils/common/success.js';
+
+const otpStore = {};
 
 export const login = async (req, res) => {
 
@@ -7,7 +12,8 @@ export const login = async (req, res) => {
 
     if (!username || !email || !password) {
         console.log("fill the required credentials");
-        return res.status(400).json({ message: "fill the required credentials" });
+        ErrorResponse.message = "fill the required credentials" 
+        return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
     }
 
     try {
@@ -19,44 +25,52 @@ export const login = async (req, res) => {
 
         if (!currentUser) {
             console.log("User Not Found");
-            return res.status(400).json({ message: "User Not Found" });
+            ErrorResponse.message = "User Not Found"
+            return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
         }
 
         const matchPass = await bcrypt.compare(password, currentUser.password)
 
         if (!matchPass) {
-            return res.status(401).json({ message: 'Invalid credentials.' });
+            ErrorResponse.message = "Invalid credentials" 
+            return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
         }
 
-        return res.status(200).json({
-            message: 'Login successful.',
+        
+        SuccessResponse.message = "Login successful";
+        SuccessResponse.data = {
             user: {
                 id: currentUser.id,
                 username: currentUser.username,
                 email: currentUser.email,
                 name: currentUser.name,
             }
-    })
+        }
+        return res.status(StatusCodes.OK).json(SuccessResponse)
     } catch (error) {
-        return res.status(400).json("Error is ", error.message);
+        ErrorResponse.message = error.message
+        return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
     }
 }
 
-export const signup = async(req,res) => {
+export const signup = async (req, res) => {
 
-    const { name,username,email,gender,password } = req.body;
+    const { name, username, email, gender, password } = req.body;
 
-    if(!username || !name || !email || !gender || !password){
-        return res.status(401).json({message:"All fields are required."})
+    if (!username || !name || !email || !gender || !password) { 
+        ErrorResponse.message = "fill all the credentials" 
+        return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
     }
-    
-    try{    
-        const existingUser = await User.findOne({where:{email:email}})
-        if(existingUser){
-            return res.status(401).json({message:"Email already existed"});
+
+    try {
+        const existingUser = await User.findOne({ where: { email: email } })
+        if (existingUser) {
+            console.log("Email already existed")
+            ErrorResponse.message = "Email already existed"
+            return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
         }
 
-        const hashedPassword = await bcrypt.hash(password,10);
+        const hashedPassword = await bcrypt.hash(password, 10);
         const user = await user.create({
             name,
             username,
@@ -65,22 +79,95 @@ export const signup = async(req,res) => {
             gender
 
         })
-        if(!user){
-            return res.status(400).json({message:"User not created"});
+        if (!user) {
+            console.log("User not created");
+            ErrorResponse.message = "User not created"
+            return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
         }
-        return res.status(200).json({
-            message: 'User registered successfully.',
+        SuccessResponse.message = "User registered successfully";
+        SuccessResponse.data = {
             user: {
-              id: user.id,
-              username: user.username,
-              email: user.email,
-              name: user.name,
+                id: user.id,
+                username: user.username,
+                email: user.email,
+                name: user.name,
+            }
+
+        }
+        return res.status(StatusCodes.OK).json(SuccessResponse);
+
+    } catch (error) {
+        console.log(error.message);
+        ErrorResponse.message = error.message
+        return res.status(StatusCodes.BAD_GATEWAY).json(ErrorResponse);
+    }
+
+
+}
+
+export const sendOtp = async (req, res) => {
+    const { email } = req.body;
+
+    try {
+        const user = await User.findOne({ where: { email: email } });
+
+        if (!user) {
+            console.log("User not found");
+            ErrorResponse.message = "User not found";
+            return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+
+        }
+
+        const otp = Math.floor(100000 + Math.random() * 900000).toString();
+
+        otpStore[email] = { otp, expiresAt: Date.now() + 5 * 60 * 1000 };
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASSWORD
             }
         })
 
-    }catch(error){
-        return res.status(500).json("error is ",error.message);
+        const mailoptions = {
+            from: process.env.EMAIL_USER,
+            to: email,
+            subject: "Your OTP for Password Reset",
+            text: `Your OTP is ${otp}. It will expire in 5 minutes.`
+        }
+
+
+        await transporter.sendMail(mailoptions);
+        SuccessResponse.message = "OTP sent successfully";
+        return res.status(StatusCodes.OK).json(SuccessResponse);
+
+    } catch (error) {
+        console.log("Cant send otp", error);
+        ErrorResponse.message = error.message;
+        return res.status(StatusCodes.BAD_GATEWAY).json(ErrorResponse)
+
     }
+}
+
+export const updatePass = async (req, res) => {
+
+    const { email,otp,newpassword } = req.body;
+    const storedOtp = otpStore[email];
+
+    if(!storedOtp || !storedOtp!=otp || Date.now() > storedOtp.expiresAt){
+        console.log("Invalid or expired OTP");
+        ErrorResponse.message = "Invalid or expired OTP";
+        return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+    }
+
+    const hashedPassword = await bcrypt.hash(password,10);
+    await User.update({password: hashedPassword},{where:{email:email}});
+
+    delete otpStore[email];
+
+    SuccessResponse.message = "Password updated successfully";
+    return res.status(StatusCodes.OK).json(SuccessResponse);
+
 
 
 }
